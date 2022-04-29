0703c9c03e7a915bf8bff9622f0f8c1f8bf0e177
Eugene <eugeny.kuz@gmail.com>
2022-04-30 01:00:49 +0500

    Base setup fixes

--- src/App.py
+++ src/App.py
@@ -1,10 +1,22 @@
-from aiohttp.web import run_app, Application
+import logging
+import os
+
+from aiohttp.web import run_app, Application, \
+    Request, Response
+from aioredis import from_url
 
 
-def init():
+def init() -> Application:
     app = Application()
+    app.router.add_route("GET", "/health", healthcheck)
+    app["red"] = from_url("redis://default:4B*Phw7KMV@redis:6379/1")
 
     return app
+
+
+async def healthcheck(request: Request) -> Response:
+    await request.app["red"].rpush("a", 33)
+    return Response(status=200)
 
 
 if __name__ == "__main__":
