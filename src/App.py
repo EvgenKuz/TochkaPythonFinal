65c7f618a89c23629af1854aa5927af0d75c2912
Eugeniy Kuznetsov <eugeny.kuz@gmail.com>
2022-04-30 15:46:28 +0500

    Final Base setup

--- src/App.py
+++ src/App.py
@@ -1,21 +1,19 @@
-import logging
 import os
 
 from aiohttp.web import run_app, Application, \
     Request, Response
-from aioredis import from_url
+from aioredis import Redis
 
 
 def init() -> Application:
     app = Application()
-    app.router.add_route("GET", "/health", healthcheck)
-    app["red"] = from_url("redis://default:4B*Phw7KMV@redis:6379/1")
+    app.router.add_route("GET", "/health", health_check)
+    app["red"] = Redis(host="redis", password=os.getenv("REDIS_PASS"))
 
     return app
 
 
-async def healthcheck(request: Request) -> Response:
-    await request.app["red"].rpush("a", 33)
+async def health_check(request: Request) -> Response:
     return Response(status=200)
 
 
